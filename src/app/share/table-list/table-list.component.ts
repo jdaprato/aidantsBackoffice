ee13c7353756a9c9cf12d428a1fe0ecef77135ff
Julie <jdaprato07@gmail.com>
2020-05-02 11:47:23 +0200

    Attempt to generate devices list from table component

--- src/app/share/table-list/table-list.component.ts
+++ src/app/share/table-list/table-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-list.component.css']
 })
 export class TableListComponent implements OnInit {
+
+  @Input() item: any;
 
   constructor() { }
 
